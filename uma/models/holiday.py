28fc7528e2d0b64c25f043e6327f534dec6d5b8a
nico <nicolas.p.suarez@gmail.com>
2019-07-25 13:25:12 -0300

    Add day type field to holiday model

--- uma/models/holiday.py
+++ uma/models/holiday.py
@@ -1,3 +1,4 @@
+from .daytype import DayType
 from django.db import models
 from django.utils import formats
 from safedelete.models import SafeDeleteModel
@@ -9,6 +10,7 @@ class Holiday(SafeDeleteModel):
     """
 
     date = models.DateField(unique=True)
+    day_type = models.ForeignKey(DayType, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return formats.date_format(self.date)
