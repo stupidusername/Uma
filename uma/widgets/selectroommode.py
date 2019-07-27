7e613328eefe0cb589aec60026da378c5b72fa12
nico <nicolas.p.suarez@gmail.com>
2019-07-27 20:04:15 -0300

    Add room mode models to the context of the SelectRoomMode widget

--- uma/widgets/selectroommode.py
+++ uma/widgets/selectroommode.py
@@ -1,3 +1,4 @@
+from ..models.roommode import RoomMode
 from django.forms.widgets import Select
 
 
@@ -6,3 +7,13 @@ class SelectRoomMode(Select):
     A select field for the room modes. It also displays the room mode
     description when an option is selected.
     """
+
+    def get_context(self, name, value, attrs):
+        """
+        Add the room mode models to the context.
+
+        :returns dict:
+        """
+        context = super().get_context(name, value, attrs)
+        context['room_modes'] = RoomMode.objects.all()
+        return context
