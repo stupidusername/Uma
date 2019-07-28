bea8c85e659645e2be02d3e56e07311317212afb
nico <nicolas.p.suarez@gmail.com>
2019-07-28 15:01:23 -0300

    Change NonEditablePriceMixin to detect changes to any field when saving

--- uma/models/abstractprice.py
+++ uma/models/abstractprice.py
@@ -24,8 +24,8 @@ class AbstractPrice(SafeDeleteModel):
 class NonEditablePriceMixin(models.Model):
     """
     Use this mixin if you want to make a price model non-editable. This means
-    that if its price gets changed it will be saved as a new record instead of
-    updating the old one.
+    that if its any field of the model gets changed it will be saved as a new
+    record instead of updating the old one.
     """
 
     class Meta:
@@ -35,12 +35,28 @@ class NonEditablePriceMixin(models.Model):
         """
         See parent doc.
         """
-        # Check if the PK was set to determine whether this in an update.
+        # If the model was changed, set its PK to None to force an insert.
+        if self.has_changed():
+            self.pk = None
+        super().save(*args, **kwargs)
+
+    def has_changed(self) -> bool:
+        """
+        Check if a any field of the model was changed but not saved.
+
+        :returns bool: `False` if the models is new or no fields were changed
+                       since the last save. `True` otherwise.
+        """
+        has_changed = False
+        # If the model does not have a PK then it is a new one.
         if self.pk:
             # Get old record from the DB.
             old_model = self.__class__._default_manager.get(pk=self.pk)
-            # Unset the PK if the price is not the same.
-            # This will force a new model creation.
-            if self.price != old_model.price:
-                self.pk = None
-        super().save(*args, **kwargs)
+            # Check if any field was changed.
+            for f in self.__class__._meta.fields:
+                # Leave the PK out of this.
+                if not f.primary_key:
+                    if getattr(self, f.name) != getattr(old_model, f.name):
+                        has_changed = True
+                        break
+        return has_changed
