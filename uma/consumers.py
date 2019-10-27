6b9533dd4f1cc7f60a4870aa6fef5de42a328cfd
noone <nicolas.p.suarez@gmail.com>
2019-10-27 18:31:33 -0300

    Add accept method to SGHConsumer

--- uma/consumers.py
+++ uma/consumers.py
@@ -8,10 +8,21 @@ class SGHConsumer(WebsocketConsumer):
     server. It implements the JSON-RPC protocol.
     """
 
+    def accept(self, subprotocol=None):
+        """
+        Accepts an incoming socket.
+
+        :param [None, str] subprotocol: Connection subprotocol (optional).
+        """
+        super().accept(subprotocol)
+
     def receive(self, text_data=None, bytes_data=None):
         """
         Called with either text_data or bytes_data for each frame. See parent
         doc.
+
+        :param str text_data:
+        :param bytes_data bytes:
         """
         # Dispatch the JSON-RPC method and get the response.
         response = JSONRPCResponseManager.handle(text_data, dispatcher)
